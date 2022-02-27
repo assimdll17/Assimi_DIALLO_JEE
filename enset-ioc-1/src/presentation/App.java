54da547820925a5f9e6a309a3190998f970ec4a7
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-02-27 16:13:44 +0100

    instanc statique et dyn

--- enset-ioc-1/src/presentation/App.java
+++ enset-ioc-1/src/presentation/App.java
@@ -1,11 +1,12 @@
 package presentation;
 
 import dao.DaoImpl;
+import ext.DaoImpl2;
 import metier.MetierImpl;
 
 public class App {
     public static void main(String[] args) {
-        DaoImpl dao = new DaoImpl();
+        DaoImpl2 dao = new DaoImpl2();
         MetierImpl metier = new MetierImpl();
         metier.setDao(dao);
         System.out.println(metier.calcul());
