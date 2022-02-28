4e4db54d258f288ca76fabf50982a11e22816bce
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-02-28 01:33:16 +0100

    instanc statique et dyn

--- TP1/enset-ioc-2/src/main/java/metier/MetierImpl.java
+++ TP1/enset-ioc-2/src/main/java/metier/MetierImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MetierImpl implements IMetier{
     @Autowired
-    @Qualifier("dao")
+    @Qualifier("daoVWS")
     private IDao dao;
 
     public void setDao(IDao dao) {
